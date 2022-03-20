79bb537e1cc07da6d92d922e3392651ebfb2ffda
supercoder <kazehazaya@gmail.com>
2022-03-20 16:46:56 +0900

    added follow function, scrollPage

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,50 +1,14 @@
 import shortId from 'shortid';
 import produce from 'immer';
+import faker from 'faker';
 
 export const initialState = {
-  mainPosts: [
-    {
-      id: shortId.generate(),
-      User: {
-        id: shortId.generate(),
-        nickname: 'supercoder',
-      },
-      content: '첫 번째 게시글 #해시태그 #익스페리스',
-      Images: [
-        {
-          id: shortId.generate(),
-          src: 'https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg',
-        },
-        {
-          id: shortId.generate(),
-          src: 'https://coinpan.com/files/attach/images/198/535/178/095/23f39ad4b20be9edd80da6ebb6804fc1.png',
-        },
-        {
-          id: shortId.generate(),
-          src: 'https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg',
-        },
-      ],
-      Comments: [
-        {
-          id: shortId.generate(),
-          User: {
-            id: shortId.generate(),
-            nickname: 'hypercoder',
-          },
-          content: 'hello world',
-        },
-        {
-          id: shortId.generate(),
-          User: {
-            id: shortId.generate(),
-            nickname: 'ultracoder',
-          },
-          content: 'bye world',
-        },
-      ],
-    },
-  ],
+  mainPosts: [],
   imagePaths: [],
+  hasMorePosts: true,
+  loadPostLoading: false,
+  loadPostDone: false,
+  loadPostError: null,
   addPostLoading: false,
   addPostDone: false,
   addPostError: null,
@@ -55,10 +19,41 @@ export const initialState = {
   addCommentDone: false,
   addCommentError: null,
 };
+
+export const generateDummyPost = (number) =>
+  Array(number)
+    .fill()
+    .map(() => ({
+      id: shortId.generate(),
+      User: {
+        id: shortId.generate(),
+        nickname: faker.name.findName(),
+      },
+      content: faker.lorem.paragraph(),
+      Images: [
+        {
+          src: faker.image.image(),
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            id: shortId.generate(),
+            nickname: faker.name.findName(),
+          },
+          content: faker.lorem.sentence(),
+        },
+      ],
+    }));
+
 // ADD_POST___
 export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
 export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
 export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
+
+export const LOAD_POSTS_REQUEST = 'LOAD_POSTS_REQUEST';
+export const LOAD_POSTS_SUCCESS = 'LOAD_POSTS_SUCCESS';
+export const LOAD_POSTS_FAILURE = 'LOAD_POSTS_FAILURE';
 
 export const REMOVE_POST_REQUEST = 'REMOVE_POST_REQUEST';
 export const REMOVE_POST_SUCCESS = 'REMOVE_POST_SUCCESS';
@@ -99,9 +94,26 @@ const dummyComment = (data) => ({
   },
 });
 
-const reducer = (state = initialState, action) => {
-  return produce(state, (draft) => {
+const reducer = (state = initialState, action) =>
+  produce(state, (draft) => {
     switch (action.type) {
+      case LOAD_POSTS_REQUEST:
+        draft.loadPostLoading = true;
+        draft.loadPostDone = false;
+        draft.loadPostError = null;
+        break;
+      case LOAD_POSTS_SUCCESS:
+        draft.loadPostLoading = false;
+        draft.loadPostDone = true;
+        draft.mainPosts = action.data.concat(draft.mainPosts);
+        draft.hasMorePosts = draft.mainPosts.length < 50;
+        break;
+      case LOAD_POSTS_FAILURE:
+        c;
+        draft.loadPostLoading = false;
+        draft.loadPostError = action.error;
+        break;
+
       // ADD_POST___
       case ADD_POST_REQUEST:
         // added immer
@@ -109,36 +121,17 @@ const reducer = (state = initialState, action) => {
         draft.addPostDone = false;
         draft.addPostError = null;
         break;
-      // return {
-      //   ...state,
-      //   addPostLoading: true,
-      //   addPostError: null,
-      //   addPostDone: false,
-      // };
       case ADD_POST_SUCCESS:
         draft.mainPosts.unshift(dummyPost(action.data));
         draft.addPostLoading = false;
-        draft.addCommentDone = true;
-        draft.addCommentError = null;
+        draft.addPostDone = true;
+        draft.addPostError = null;
         break;
-      // return {
-      //   ...state,
-      //   mainPosts: [dummyPost(action.data), ...state.mainPosts],
-      //   addPostLoading: false,
-      //   addPostError: null,
-      //   addPostDone: true,
-      // };
       case ADD_POST_FAILURE:
-        draft.addCommentLoading = false;
+        draft.addPostLoading = false;
         draft.addPostDone = false;
         draft.addPostError = action.error;
         break;
-      // return {
-      //   ...state,
-      //   addPostLoading: false,
-      //   addPostError: action.error,
-      //   addPostDone: false,
-      // };
 
       // REMOVE_POST___
       case REMOVE_POST_REQUEST:
@@ -146,36 +139,19 @@ const reducer = (state = initialState, action) => {
         draft.removePostError = null;
         draft.removePostDone = false;
         break;
-      // return {
-      //   ...state,
-      //   removePostLoading: true,
-      //   removePostError: null,
-      //   removePostDone: false,
-      // };
+
       case REMOVE_POST_SUCCESS:
         draft.mainPosts = draft.mainPosts.filter((v) => v.id !== action.data);
         draft.removePostLoading = false;
         draft.removePostError = null;
         draft.removePostDone = true;
         break;
-      // return {
-      //   ...state,
-      //   mainPosts: state.mainPosts.filter((v) => v.id !== action.data),
-      //   removePostLoading: false,
-      //   removePostError: null,
-      //   removePostDone: true,
-      // };
+
       case REMOVE_POST_FAILURE:
         draft.removePostLoading = false;
         draft.removePostError = action.error;
         draft.removePostDone = false;
         break;
-      // return {
-      //   ...state,
-      //   removePostLoading: false,
-      //   removePostError: action.error,
-      //   removePostDone: false,
-      // };
 
       // ADD_COMMENT___
       case ADD_COMMENT_REQUEST:
@@ -183,12 +159,6 @@ const reducer = (state = initialState, action) => {
         draft.addCommentError = null;
         draft.addCommentDone = false;
         break;
-      // return {
-      //   ...state,
-      //   addCommentLoading: true,
-      //   addCommentError: null,
-      //   addCommentDone: false,
-      // };
       case ADD_COMMENT_SUCCESS:
         const post = draft.mainPosts.find((v) => v.id === action.data.postId);
         post.Comments.unshift(dummyComment(action.data.content));
@@ -196,39 +166,16 @@ const reducer = (state = initialState, action) => {
         draft.addCommentError = null;
         draft.addCommentDone = true;
         break;
-      //   {
-      //     const postIndex = state.mainPosts.findIndex(
-      //     (v) => v.id === action.data.postId
-      //   );
-      //   const post = { ...state.mainPosts[postIndex] };
-      //   const Comments = [dummyComment(action.data.content), ...post.Comments];
-      //   post.Comments = post.Comments.slice();
-      //   const mainPosts = [...state.mainPosts];
-      //   mainPosts[postIndex] = { ...post, Comments };
-      //   return {
-      //     ...state,
-      //     mainPosts,
-      //     addCommentLoading: false,
-      //     addCommentError: null,
-      //     addCommentDone: true,
-      //   };
-      // }
+
       case ADD_COMMENT_FAILURE:
         draft.addCommentLoading = false;
         draft.addCommentError = action.error;
         draft.addCommentDone = false;
         break;
-      // return {
-      //   ...state,
-      //   addCommentLoading: false,
-      //   addCommentError: action.error,
-      //   addCommentDone: false,
-      // };
+
       default:
         break;
-      // return state;
     }
   });
-};
 
 export default reducer;
