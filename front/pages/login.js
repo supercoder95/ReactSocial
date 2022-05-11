1ccf3812d7d232eb4e4d9df809d889e1e97b8ddb
supercoder <kazehazaya@gmail.com>
2022-05-11 14:32:15 +0900

    logIn fixed

--- front/pages/login.js
+++ front/pages/login.js
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Router from 'next/router';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import AppLayout from '../components/AppLayout';
 import LoginForm from '../components/LoginForm';
 
